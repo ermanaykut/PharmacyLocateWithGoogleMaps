8f7d832a8adaab923195bbd78e6e0a8c50d2dd91
ermanaykut <109579055+ermanaykut@users.noreply.github.com>
2023-03-25 13:16:14 +0300

    Update index.ts

--- src/config/environments/index.ts
+++ src/config/environments/index.ts
@@ -1,8 +1,1 @@
-const GOOGLE_API_KEY: string = 'AIzaSyChY5Rv9WTYejkzjkV0bG3KPqJFB10qwCg';
 
-const NOSYA_API_KEY: string =
-  'VpSyeow88wCnrtFHtbl9wi0fTY0Y9lQ89nh8dSiy4uXpTF7i8OkTkLO785Rv';
-
-const OSM_API_KEY: string =
-  '5b3ce3597851110001cf62485f345818dacd4cbda0b365c7c86fb074';
-export {GOOGLE_API_KEY, NOSYA_API_KEY, OSM_API_KEY};
